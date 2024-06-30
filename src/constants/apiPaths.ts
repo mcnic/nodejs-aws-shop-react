09b9d99cf355b2ba1968736b113c16a960ed302c
max <mcnic@users.noreply.github.com>
2024-06-30 08:50:44 -0700

    feat: fix Import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const prefix = import.meta.env.VITE_API_PREFIX || ".execute-api2";
-const awsAPI = "https://o2jh3m7b57.execute-api.us-east-1.amazonaws.com/prod";
 
 const API_PATHS = {
-  product: awsAPI,
+  product: "https://o2jh3m7b57.execute-api.us-east-1.amazonaws.com/prod",
   order: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
-  import: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
+  import: "https://906nuso9ee.execute-api.us-east-1.amazonaws.com/prod",
   bff: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
   cart: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
 };
