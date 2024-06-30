56f39dea9edb0ebb5a08433abb0c774dda7541b9
max <mcnic@users.noreply.github.com>
2024-06-30 14:18:43 -0700

    fix: service URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const prefix = import.meta.env.VITE_API_PREFIX || ".execute-api2";
 const API_PATHS = {
   product: "https://o2jh3m7b57.execute-api.us-east-1.amazonaws.com/prod",
   order: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
-  import: "https://906nuso9ee.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://i6gsu0m0sg.execute-api.us-east-1.amazonaws.com/prod",
   bff: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
   cart: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
 };
