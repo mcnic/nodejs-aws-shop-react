5b4f614bf5d0f764a37804b992d6ec500ed24036
max <mcnic@users.noreply.github.com>
2024-07-14 17:41:20 -0700

    feat: add axos interceptor

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,25 @@ if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
 }
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error(error);
+
+    switch (error.response.status) {
+      case 401:
+        alert("401 Unauthorized error");
+        break;
+      case 403:
+        alert("403 Forbidden error");
+        break;
+      default:
+        alert(error);
+    }
+    return error;
+  }
+);
 
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
